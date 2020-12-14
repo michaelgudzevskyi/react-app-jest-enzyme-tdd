b02472ece9e58e5c314cc8826af15b52970a2518
Mykhailo Hudzevatyi <gudzevatyi@gmail.com>
2020-12-14 14:46:42 -0300

    seup Redux

---
+++ src/reducers/successReducer.js
@@ -0,0 +1,10 @@
+/**
+ * @function successReducer
+ * @param {array} state - Array of guessed words.
+ * @param {array} action - Action to be reduced.
+ * @returns {JSX.Element} - New success state.
+ */
+
+export default (state, action) => {
+    return null
+}
