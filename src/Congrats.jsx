b3593c644b93e6f3bcfb3a73a5e16989bfb0b2fe
Mykhailo Hudzevatyi <gudzevatyi@gmail.com>
2020-12-14 14:11:47 -0300

    Setup branch

--- src/Congrats.jsx
+++ src/Congrats.jsx
@@ -9,8 +9,9 @@ import PropTypes from 'prop-types';
 const Congrats = props => {
     return (
         props.success ? <div data-test="component-congrats" className="alert alert-success">
-            <span data-test="congrats-message" >
+            <span data-test="congrats-message">
                 Congratulations! You guessed the word!
+                
             </span>
         </div> : <div data-test="component-congrats" />
     );
