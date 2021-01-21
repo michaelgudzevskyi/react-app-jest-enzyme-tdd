f5cc8c3a6d925560c20a670d4a8972acaac903a7
Michael Gudzevskyi <hello@michaelgudzevskyi.com>
2021-01-20 22:05:20 -0300

    Set uup redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
+import { Component } from 'react'
 import Congrats from "./Congrats"
 import GuessedWords from "./GuessedWords"
 
-function App() {
-  return (
-    <div className="container">
-      <h1>Jotto</h1>
-      <Congrats success={true}/>
-      <GuessedWords guessedWords={[ { guessedWord: 'train', letterMatchCount: 3 },
-       { guessedWord: 'agile', letterMatchCount: 1 },
-       { guessedWord: 'party', letterMatchCount: 5 }]}/>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>Jotto</h1>
+        <Congrats success={true}/>
+        <GuessedWords guessedWords={[
+          { guessedWord: 'train', letterMatchCount: 3 }
+        ]}/>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
