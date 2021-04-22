49e8b3031482040339990e81dd217b269fb656e9
Michael Gudzevskyi <hello@michaelgudzevskyi.com>
2021-04-22 18:26:53 -0300

    Before eject

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
-    <div className="App">
-       <h1>Learn React Testing</h1>
+    <div data-test="component-app" className="App">
+      {/* <h1 data-test="counter-display">
+        The counter is currently&nbsp; 
+        <span data-test="count">{count}</span>
+      </h1>
+       <button
+        data-test="increment-button"
+        onClick={() => setCount(count + 1)}
+      >
+        Increment counter
+      </button> */}
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,30 @@
-import { render, screen } from '@testing-library/react';
+import Enzyme, { shallow } from 'enzyme'
+import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17'
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({ adapter: new EnzymeAdapter() })
+
+test('renders non-empty component without crashing', () => {
+  const wrapper = shallow(<App />)
+   const appComponent = wrapper.find("[data-test='component-app']")
+   expect(appComponent.length).toBe(1)
 });
+
+test('renders button ', () => {
+  
+})
+
+test('renders counter display', () => {
+  
+})
+
+test('counter display starts at 0', () => {
+  
+})
+
+test('clicking button increments counter display', () => {
+  
+})
+
+
+
