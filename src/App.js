60b0b23aab9ec4d3a3c8c5784d3690ca6a287d50
Michael Gudzevskyi <hello@michaelgudzevskyi.com>
2021-04-23 13:38:18 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -2,20 +2,11 @@ import {useState} from 'react';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+ 
 
   return (
     <div data-test="component-app" className="App">
-      {/* <h1 data-test="counter-display">
-        The counter is currently&nbsp; 
-        <span data-test="count">{count}</span>
-      </h1>
-       <button
-        data-test="increment-button"
-        onClick={() => setCount(count + 1)}
-      >
-        Increment counter
-      </button> */}
+ 
     </div>
   );
 }
